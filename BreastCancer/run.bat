b70e19db3f080d1d89a5515dfa4539cd220c2457
sinab <baharlou@usc.edu>
2021-08-13 00:16:21 -0700

    + delete unnecessary files

--- BreastCancer/run.bat
+++
@@ -1,5 +0,0 @@
-python test.py 1
-python test.py 2
-python test.py 3
-python test.py 4
-python test.py 5
--- BreastCancer/run2.bat
+++
@@ -1,5 +0,0 @@
-python test2.py 1
-python test2.py 2
-python test2.py 3
-python test2.py 4
-python test2.py 5
